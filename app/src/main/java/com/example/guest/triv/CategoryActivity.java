5aee7985134ecc643d42ecf07703caecff8ed1b9
Matt Nicholas <fostermatt82@gmail.com>
2016-11-18 10:20:04 -0800

    add categoty buttons to category activity

--- app/src/main/java/com/example/guest/triv/CategoryActivity.java
+++ app/src/main/java/com/example/guest/triv/CategoryActivity.java
@@ -2,12 +2,36 @@ package com.example.guest.triv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CategoryActivity extends AppCompatActivity {
+    @Bind(R.id.sportsButton) Button mSportsButton;
+    @Bind(R.id.entertainmentButton) Button mEntertainmentButton;
+    @Bind(R.id.historyButton) Button mHistoryButton;
+    @Bind(R.id.politicsButton) Button mPoliticsButton;
+    @Bind(R.id.animalsButton) Button animalsButton;
+    @Bind(R.id.geographyButton) Button mGeographyButton;
+    @Bind(R.id.randomButton) Button mRandomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        ButterKnife.bind(this);
+
+        mSportsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, "This category is still under construction", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
