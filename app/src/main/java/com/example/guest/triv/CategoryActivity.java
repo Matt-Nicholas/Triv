9409671d79fbf1778ef9fbf9ed9ae215f9476250
Matt Nicholas <fostermatt82@gmail.com>
2016-11-18 10:56:05 -0800

    add on click listeners for category buttons

--- app/src/main/java/com/example/guest/triv/CategoryActivity.java
+++ app/src/main/java/com/example/guest/triv/CategoryActivity.java
@@ -12,11 +12,15 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class CategoryActivity extends AppCompatActivity {
+    // Private variable declarations
+   private static final String UNDER_CONSTRUCTION = "This category is still under construction";
+
+    //Bind views using ButtKnife
     @Bind(R.id.sportsButton) Button mSportsButton;
     @Bind(R.id.entertainmentButton) Button mEntertainmentButton;
     @Bind(R.id.historyButton) Button mHistoryButton;
     @Bind(R.id.politicsButton) Button mPoliticsButton;
-    @Bind(R.id.animalsButton) Button animalsButton;
+    @Bind(R.id.animalsButton) Button mAnimalsButton;
     @Bind(R.id.geographyButton) Button mGeographyButton;
     @Bind(R.id.randomButton) Button mRandomButton;
 
@@ -26,10 +30,48 @@ public class CategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_category);
         ButterKnife.bind(this);
 
+
+        // Set on click listeners and execute actions for category buttons
         mSportsButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(CategoryActivity.this, "This category is still under construction", Toast.LENGTH_LONG).show();
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mEntertainmentButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mHistoryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mPoliticsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mAnimalsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mGeographyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, UNDER_CONSTRUCTION, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mRandomButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(CategoryActivity.this, "Random", Toast.LENGTH_LONG).show();
             }
         });
 
