a63657cb786bc4640a240b5616dbf148fec68e65
Matt Nicholas <fostermatt82@gmail.com>
2016-12-01 19:53:14 -0800

    increase margin between buttons for fat fingers like mine

--- app/src/main/java/com/example/guest/triv/ui/QuizActivity.java
+++ app/src/main/java/com/example/guest/triv/ui/QuizActivity.java
@@ -1,6 +1,5 @@
 package com.example.guest.triv.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,21 +11,24 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import java.io.IOException;
 import com.example.guest.triv.R;
+import com.example.guest.triv.models.PastQuestion;
 import com.example.guest.triv.models.Question;
 import com.example.guest.triv.services.TriviaService;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import okhttp3.Response;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String TAG = QuizActivity.class.getSimpleName();
     // Private variable declarations
     private static final String CORRECT = "Correct!";
     private static final String INCORRECT = "Wrong!";
     private ArrayList<String> mPastQuestions = new ArrayList<>();
     private ArrayList<Question> mQuestions = new ArrayList<>();
+    List<String> allAnswers = new ArrayList<>();
 
     //Bind views using ButtKnife
     @Bind(R.id.categoryView) TextView mCategoryView;
@@ -41,9 +43,72 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         ButterKnife.bind(this);
+
+        // Set on click listeners
+        mAnswerButton0.setOnClickListener(this);
+        mAnswerButton1.setOnClickListener(this);
+        mAnswerButton2.setOnClickListener(this);
+        mAnswerButton3.setOnClickListener(this);
+
 //        Intent intent = getIntent();
 //        String selectedCategory = intent.getStringExtra("category");
         getQuestions();
+    }
+      // Execute actions depending on which onClick listener is triggered
+    @Override
+    public void onClick(View v){
+        String selectedAnswer;
+        if(v == mAnswerButton0){
+            selectedAnswer = (String) mAnswerButton0.getText();
+            if(answerIsCorrect(selectedAnswer, 0)){
+                Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton0.setBackgroundColor(0xff00ff00);
+                getQuestions();
+            }else{
+                getQuestions();
+                Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton0.setBackgroundColor(0xffff0000);
+            }
+        } else if(v == mAnswerButton1){
+            selectedAnswer = (String) mAnswerButton1.getText();
+            if(answerIsCorrect(selectedAnswer, 1)){
+                Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton1.setBackgroundColor(0xff00ff00);
+                getQuestions();
+            }else{
+                getQuestions();
+                Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton1.setBackgroundColor(0xffff0000);
+            }
+        }else if(v == mAnswerButton2){
+            selectedAnswer = (String) mAnswerButton2.getText();
+            if(answerIsCorrect(selectedAnswer, 2)){
+                Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton2.setBackgroundColor(0xff00ff00);
+                getQuestions();
+            }else{
+                getQuestions();
+                Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton2.setBackgroundColor(0xffff0000);
+            }
+        }else if(v == mAnswerButton3){
+            selectedAnswer = (String) mAnswerButton3.getText();
+            if(answerIsCorrect(selectedAnswer, 3)){
+                Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton3.setBackgroundColor(0xff00ff00);
+                getQuestions();
+            }else{
+                getQuestions();
+                Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
+                mAnswerButton3.setBackgroundColor(0xffff0000);
+            }
+        }
+    }
+    public boolean answerIsCorrect(String a, int i){
+        if (allAnswers.get(i).equals(a)){
+            return true;
+        }
+        return false;
     }
 
     private void getQuestions() {
@@ -64,10 +129,12 @@ public class QuizActivity extends AppCompatActivity {
                 });
             }
         });
-
     }
 
     public void setNewQuestion(String category, String type, String difficulty, String question, String correctAnswer, ArrayList<String> incorrectAnswer){
+
+        // Make a copy of incorrect Answers array list
+        allAnswers = new ArrayList<>(incorrectAnswer);
         final Question newQuestion = new Question(category, type, difficulty, question, correctAnswer, incorrectAnswer);
 
         // Creates a new random number generator
@@ -80,121 +147,24 @@ public class QuizActivity extends AppCompatActivity {
         // Creates a new random int from 0-3
         int randInt = randGen.nextInt(4);
 
-        // ****** NEEDS TO BE REFACTORED NOT DRY ******
+        // reset background color to white on all buttons
+        allAnswers.add(randInt, correctAnswer);
+        mAnswerButton0.setBackgroundColor(0xffffffff);
+        mAnswerButton1.setBackgroundColor(0xffffffff);
+        mAnswerButton2.setBackgroundColor(0xffffffff);
+        mAnswerButton3.setBackgroundColor(0xffffffff);
+
         // Sets text on buttons to possible answers
-        for (int i = 0; i < 4; i++) {
-            if(randInt == 0){
-                mAnswerButton0.setText(newQuestion.getCorrectAnswer()); // Sets button text to the correct answer if randInt returns a 0
-                mAnswerButton0.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), true); // Can't pass custom object as Extra?
-                        mPastQuestions.add(newQuestion.getQuestion());
-                    }
-                });
-            }else{
-                mAnswerButton0.setText(newQuestion.getIncorrectAnswers().get(0)); // Sets button text to incorrect answer if randInt returns something other than 0
-                mAnswerButton0.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), false); // Can't pass custom object as Extra?
-//                        mPastQuestions.add(newQuestion.getQuestion());
-//                        Intent intent = new Intent(QuizActivity.this, PastQuestionsActivity.class);
-//                        intent.putExtra("pastQuestions", mPastQuestions);
-//                        startActivity(intent);
-                    }
-                });
-            }
-            if(randInt == 1){
-                mAnswerButton1.setText(newQuestion.getCorrectAnswer());// Sets button text to the correct answer if randInt returns a 1
-                mAnswerButton1.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), true); // Can't pass custom object as Extra?
-                        mPastQuestions.add(newQuestion.getQuestion());
-                    }
-                });
-            }else{
-                mAnswerButton1.setText(newQuestion.getIncorrectAnswers().get(1)); // Sets button text to incorrect answer if randInt returns something other than 1
-                mAnswerButton1.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), false); // Can't pass custom object as Extra?
-//                        mPastQuestions.add(newQuestion.getQuestion());
-//                        Intent intent = new Intent(QuizActivity.this, PastQuestionsActivity.class);
-//                        intent.putExtra("pastQuestions", mPastQuestions);
-//                        startActivity(intent);
-                    }
-                });
-            }
-            if(randInt == 2){
-                mAnswerButton2.setText(newQuestion.getCorrectAnswer());// Sets button text to the correct answer if randInt returns a 2
-                mAnswerButton2.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), true);// Can't pass custom object as Extra?
-                        mPastQuestions.add(newQuestion.getQuestion());
-                    }
-                });
-            }else{
-                mAnswerButton2.setText(newQuestion.getIncorrectAnswers().get(2)); // Sets button text to incorrect answer if randInt returns something other than 2
-                mAnswerButton2.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), false);// Can't pass custom object as Extra?
-//                        mPastQuestions.add(newQuestion.getQuestion());
-//                        Intent intent = new Intent(QuizActivity.this, PastQuestionsActivity.class);
-//                        intent.putExtra("pastQuestions", mPastQuestions);
-//                        startActivity(intent);
-                    }
-                });
-            }
-            if(randInt == 3){
-                mAnswerButton3.setText(newQuestion.getCorrectAnswer());// Sets button text to the correct answer if randInt returns a 3
-                mAnswerButton3.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, CORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), true);// Can't pass custom object as Extra?
-                        mPastQuestions.add(newQuestion.getQuestion());
-                    }
-                });
-            }else{
-                mAnswerButton3.setText(newQuestion.getIncorrectAnswers().get(2)); // Sets button text to incorrect answer if randInt returns something other than 3
-                mAnswerButton3.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        getQuestions();
-
-                        Toast.makeText(QuizActivity.this, INCORRECT, Toast.LENGTH_SHORT).show();
-//                        PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), false);// Can't pass custom object as Extra?
-//                        mPastQuestions.add(newQuestion.getQuestion());
-//                        Intent intent = new Intent(QuizActivity.this, PastQuestionsActivity.class);
-//                        intent.putExtra("pastQuestions", mPastQuestions);
-//                        startActivity(intent);
-                    }
-                });
-            }
-        }
+        mAnswerButton0.setText(allAnswers.get(0));
+        mAnswerButton1.setText(allAnswers.get(1));
+        mAnswerButton2.setText(allAnswers.get(2));
+        mAnswerButton3.setText(allAnswers.get(3));
     }
 }
+
+
+//    PastQuestion newPastQuestion = new PastQuestion(newQuestion, newQuestion.getCorrectAnswer(), false); // Can't pass custom object as Extra?
+//                        mPastQuestions.add(newQuestion.getQuestion());
+//                        Intent intent = new Intent(QuizActivity.this, PastQuestionsActivity.class);
+//                        intent.putExtra("pastQuestions", mPastQuestions);
+//                        startActivity(intent);
